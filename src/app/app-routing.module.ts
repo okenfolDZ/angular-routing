3ea839117832b1b4752e36257d7df7c69443fda1
hamary.roland <roland.hamary@gmail.com>
2023-07-27 10:19:50 +0200

    anyag

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'team-list', component: TeamListComponent},
+  {path: 'team-details/:id', component: TeamDetailsComponent},
+  {path: '**', redirectTo: '/home'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
